2764bfbb7d2973c4a1cb5e1a8c0cec28b55a3190
stepanmk <smiklanek@gmail.com>
2023-05-25 14:41:36 +0200

    update title

--- listening-site/src/App.vue
+++ listening-site/src/App.vue
@@ -5,7 +5,7 @@
 
 <div id="main-container" class="min-h-screen flex items-start justify-center p-10">
 
-    <p class="text-3xl font-semibold">Neural Grey-Box Amplifier Modelling with Limited Data</p>
+    <p class="text-3xl font-semibold">Neural Grey-Box Guitar Amplifier Modelling with Limited Data</p>
 
 </div>
 
