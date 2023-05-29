9f71f3a981a2bad5ac531cc74a264779cd2894bb
Štěpán Miklánek <smiklanek@gmail.com>
2023-05-29 03:13:43 +0200

    listening

--- listening-site/src/App.vue
+++ listening-site/src/App.vue
@@ -1,5 +1,88 @@
 <script setup>
 import { Icon } from '@iconify/vue';
+import { ref, onMounted } from 'vue'
+import ModelNames from './components/ModelNames.vue';
+
+
+// let sound = document.createElement('audio');
+// sound.id = 'audio-player';
+// sound.controls = 'controls';
+// sound.src = './assets/audio/B1_M3_T7/1_target_LSTM40_pwr_GRU8_B1_M3_T7_G5_file_9.wav';
+
+onMounted(() => {
+    addAllSounds();
+})
+
+
+function addAllSounds()
+{    
+    let selectedFiles = [4, 5, 8, 1, 2, 3];
+    addSetting(selectedFiles, 'B1_M3_T7', 'setting-1');
+    addSetting(selectedFiles, 'B10_M0_T0', 'setting-2');
+
+}
+
+function addSetting(selectedFiles, settingName, elementName)
+{
+    const models = [
+        '1_target_LSTM48_rnn_only',
+        '3_targets_LSTM48_rnn_only',
+        '21_targets_LSTM48_rnn_only_fin',
+        '1_target_LSTM40_pwr_GRU8',
+        '3_targets_LSTM40_pwr_GRU8',
+    ]
+    let setting = document.getElementById(elementName);
+    selectedFiles.forEach((file, idx) => {
+        let sampleNumber = document.createElement('div');
+        sampleNumber.innerText = `Sample ${idx + 1}`;
+        sampleNumber.style.display = 'flex';
+        sampleNumber.style.justifyContent = 'center';
+        sampleNumber.style.alignItems = 'center';
+        setting.appendChild(sampleNumber);
+        
+        let inputDiv = document.createElement('div');
+        inputDiv.style.display = 'flex';
+        inputDiv.style.justifyContent = 'center';
+        inputDiv.style.alignItems = 'center';
+        setting.appendChild(inputDiv);
+
+        let inputSound = document.createElement('audio');
+        inputSound.src = `./src/assets/audio/${settingName}/${settingName}_G5_file_${file}_input.wav`
+        inputSound.controls = 'controls';
+        inputSound.style.width = '7rem';
+        inputSound.style.height = '2rem';
+        inputDiv.appendChild(inputSound);
+        
+        let refDiv = document.createElement('div');
+        refDiv.style.display = 'flex';
+        refDiv.style.justifyContent = 'center';
+        refDiv.style.alignItems = 'center';
+        setting.appendChild(refDiv);
+
+        let refSound = document.createElement('audio');
+        refSound.src = `./src/assets/audio/${settingName}/${settingName}_G5_file_${file}.wav`
+        refSound.controls = 'controls';
+        refSound.style.width = '7rem';
+        refSound.style.height = '2rem';
+        refDiv.appendChild(refSound);
+
+        models.forEach((modelName) => {
+            let modelDiv = document.createElement('div');
+            modelDiv.style.display = 'flex';
+            modelDiv.style.justifyContent = 'center';
+            modelDiv.style.alignItems = 'center';
+            setting.appendChild(modelDiv);
+
+            let modelSound = document.createElement('audio');
+            modelSound.src = `./src/assets/audio/${settingName}/${modelName}_${settingName}_G5_file_${file}.wav`
+            modelSound.controls = 'controls';
+            modelSound.style.width = '7rem';
+            modelSound.style.height = '2rem';
+            modelDiv.appendChild(modelSound);
+        })
+    })
+}
+
 
 </script>
 
@@ -7,13 +90,13 @@ import { Icon } from '@iconify/vue';
 
 <div id="main-container" class="min-h-screen flex justify-center">
 
-    <div id="content" class="flex flex-col items-start justify-start p-8 gap-2 xl:px-[18rem] max-w-[120rem]">
+    <div id="content" class="flex flex-col items-start justify-start p-8 gap-2 px-[22rem] max-w-[120rem]">
 
         <p class="text-4xl font-semibold">Neural Grey-Box Guitar Amplifier Modelling with Limited Data</p>
 
         <p class="w-full">Štěpán Miklánek, Alec Wright, Vesa Välimäki and Jiří Schimmel</p>
 
-        <p class="w-full font-semibold mt-4">Abstract</p>
+        <p class="w-full font-semibold mt-4 text-lg">Abstract</p>
         
         <p class="text-justify">This paper combines recurrent neural networks 
         (RNNs) with the discretised Kirchhoff nodal analysis (DK-method) to create a
@@ -31,26 +114,32 @@ import { Icon } from '@iconify/vue';
         model the linear and nonlinear parts of a guitar amplifier using
         supervised learning.</p>
 
-        <p class="w-full font-semibold mt-4">Resources</p>
+        <p class="w-full font-semibold mt-4 text-lg">Resources</p>
         <div class="w-full flex flex-col xl:flex-row gap-4">
-            <a href="" class="pl-2 pr-4 h-10 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
-                <Icon icon="ph:file-text" width="32"/>
-                <p class="font-semibold">Paper</p>
+            <a href="" class="pl-2 pr-4 h-9 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
+                <Icon icon="ph:file-text" width="26"/>
+                <p class="">Paper</p>
             </a>
 
-            <a href="https://github.com/stepanmk/grey-box-amp/tree/master/python" class="pl-2 pr-4 h-10 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
-                <Icon icon="ph:github-logo" width="32"/>
-                <p class="font-semibold">Code</p>
+            <a href="https://github.com/stepanmk/grey-box-amp/tree/master/python" class="pl-2 pr-4 h-9 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
+                <Icon icon="ph:github-logo" width="26"/>
+                <p class="">Code</p>
             </a>
             
-            <a href="https://zenodo.org/record/7970723" class="pl-2 pr-4 h-10 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
-                <Icon icon="ph:file-audio" width="32"/>
-                <p class="font-semibold">Dataset</p>
+            <a href="https://zenodo.org/record/7970723" class="pl-2 pr-4 h-9 flex flex-row items-center justify-center gap-1 bg-neutral-600 rounded-lg hover:bg-neutral-900 text-white">
+                <Icon icon="ph:file-audio" width="26"/>
+                <p class="">Dataset</p>
             </a>
         </div>
 
-        <p class="w-full font-semibold mt-4">Listening examples</p>
-
+        <p class="w-full font-semibold mt-4 text-lg">Listening examples (unseen tone stack settings)</p>
+        
+        <ModelNames title="Bass: 1, Middle: 3, Treble: 7 (MUSHRA setting 1)"></ModelNames>
+        <div id="setting-1" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
+        
+        <ModelNames title="Bass: 10, Middle: 0, Treble: 0 (MUSHRA setting 2)"></ModelNames>
+        <div id="setting-2" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>     
+    
     </div>
 </div>
 
---
+++ listening-site/src/components/ModelNames.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    title: String,
+})
+
+
+</script>
+
+<template>
+    <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">{{ title }}</p>
+    <div class="w-full flex flex-row justify-around text-white">
+        <p class="w-[7rem]"></p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-600">Input</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-red-600">Reference</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-green-600">RNN1</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-green-600">RNN3</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-green-600">RNN21</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-blue-600">TS1</p>
+        <p class="flex items-center justify-center rounded-md w-[7rem] bg-blue-600">TS3</p>
+    </div>
+</template>
