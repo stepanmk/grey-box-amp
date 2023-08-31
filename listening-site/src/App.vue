096d5be3181c22e7055db728acea5b1cd61f24fd
Štěpán Miklánek <smiklanek@gmail.com>
2023-08-31 19:12:53 +0200

    update listening site

--- listening-site/src/App.vue
+++ listening-site/src/App.vue
@@ -129,9 +129,9 @@ function addSetting(selectedFiles, settingName, elementName, models)
 
 <template>
 
-<div id="main-container" class="min-h-screen flex justify-center">
+<div id="main-container" class="overflow-x-hidden lg:flex  lg:justify-center lg:items-center">
 
-    <div id="content" class="flex flex-col items-start justify-start p-8 gap-2 max-w-[75rem]">
+    <div id="content" class="flex flex-col items-start justify-center p-8 gap-2 max-w-[80rem]">
 
         <p class="text-4xl font-semibold">Neural Grey-Box Guitar Amplifier Modelling with Limited Data</p>
 
@@ -172,109 +172,111 @@ function addSetting(selectedFiles, settingName, elementName, models)
                 <p class="">Dataset</p>
             </a>
         </div>
-
-        <p class="w-full font-semibold mt-4 text-lg">Listening examples (unseen tone stack settings)</p>
-
-        <p class="text-justify">The baseline RNN model is an LSTM of hidden size 48. The TS model (proposed) 
-            is composed of an LSTM of hidden size 40, followed by a white-box tone stack model and a GRU of hidden size 8.
-            The numbering denotes the number of tone stack settings the models have seen during training.
-            A single tone stack setting corresponds to 4 minutes of audio used during training.
-            The dataset was recorded using the speaker output of the Marshall JVM 410H guitar amplifier connected to a reactive load.
-            A speaker cabinet impulse response was not applied to the presented sounds.
-        </p>
         
-        <ModelNames title="Bass: 1, Middle: 3, Treble: 7 (MUSHRA setting 1)"></ModelNames>
-        <div id="setting-1" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
-        
-        <ModelNames title="Bass: 10, Middle: 0, Treble: 0 (MUSHRA setting 2)"></ModelNames>
-        <div id="setting-2" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
-        
-        <ModelNames title="Bass: 3, Middle: 7, Treble: 1"></ModelNames>
-        <div id="setting-3" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>  
-
-        <ModelNames title="Bass: 0, Middle: 0, Treble: 10"></ModelNames>
-        <div id="setting-4" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>  
-
-        <ModelNames title="Bass: 10, Middle: 0, Treble: 10"></ModelNames>
-        <div id="setting-5" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
-        
-        <p class="w-full font-semibold mt-4 text-lg">Listening examples (further evaluation of the proposed model)</p>
-        <p class="text-justify">The following examples show that the proposed model generalises well to additional unseen tone stack permutations. 
-            The TS1 model was trained only on a single tone stack setting (Bass: 5, Middle: 5, Treble: 5), whereas the TS3 model was trained on three
-            tone stack permutations (Bass: 0, Middle: 0, Treble: 0), (Bass: 5, Middle: 5, Treble: 5), and (Bass: 10, Middle: 10, Treble: 10).
-            This corresponds to 4 minutes of training data for the TS1 model and 12 minutes of training data for the TS3 model.
-        </p>
-
-        <!-- bass -->
-        <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Bass control varied from 0 to 10, with Middle and Treble controls set to 5 in all cases.</p>
-        <div class="w-full flex flex-row justify-around">
-            <p class="w-[7rem]"></p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 0</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 2</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 4</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 6</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 8</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 10</p>
+        <div id="mobile-only" class="lg:hidden font-semibold mt-5 text-lg">
+            The listening examples only work on larger screens. Try to use landscape orientation.
         </div>
+        <div id="hideable" class="hidden lg:flex flex-col items-start justify-start gap-2">
+            <p class="w-full font-semibold mt-4 text-lg">Listening examples (unseen tone stack settings)</p>
 
-        <div id="setting-6" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
-        </div>
+            <p class="text-justify">The baseline RNN model is an LSTM of hidden size 48. The TS model (proposed) 
+                is composed of an LSTM of hidden size 40, followed by a white-box tone stack model and a GRU of hidden size 8.
+                The numbering denotes the number of tone stack settings the models have seen during training.
+                A single tone stack setting corresponds to 4 minutes of audio used during training.
+                The dataset was recorded using the speaker output of the Marshall JVM 410H guitar amplifier connected to a reactive load.
+                A speaker cabinet impulse response was not applied to the presented sounds.
+            </p>
+            
+            <ModelNames title="Bass: 1, Middle: 3, Treble: 7 (MUSHRA setting 1)"></ModelNames>
+            <div id="setting-1" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
+            
+            <ModelNames title="Bass: 10, Middle: 0, Treble: 0 (MUSHRA setting 2)"></ModelNames>
+            <div id="setting-2" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
+            
+            <ModelNames title="Bass: 3, Middle: 7, Treble: 1"></ModelNames>
+            <div id="setting-3" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>  
 
-        <div id="setting-7" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
-        </div>
-        
-        <div id="setting-8" class="w-full flex flex-row justify-around items-center border-b pb-2">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
-        </div>
+            <ModelNames title="Bass: 0, Middle: 0, Treble: 10"></ModelNames>
+            <div id="setting-4" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>  
 
-        <!-- mid -->
-        <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Middle control varied from 0 to 10, with Bass and Treble controls set to 5 in all cases.</p>
-        <div class="w-full flex flex-row justify-around">
-            <p class="w-[7rem]"></p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 0</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 2</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 4</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 6</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 8</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 10</p>
-        </div>
+            <ModelNames title="Bass: 10, Middle: 0, Treble: 10"></ModelNames>
+            <div id="setting-5" class="w-full grid grid-cols-8 gap-y-2 border-b pb-2"></div>
+            
+            <p class="w-full font-semibold mt-4 text-lg">Listening examples (further evaluation of the proposed model)</p>
+            <p class="text-justify">The following examples show that the proposed model generalises well to additional unseen tone stack permutations. 
+                The TS1 model was trained only on a single tone stack setting (Bass: 5, Middle: 5, Treble: 5), whereas the TS3 model was trained on three
+                tone stack permutations (Bass: 0, Middle: 0, Treble: 0), (Bass: 5, Middle: 5, Treble: 5), and (Bass: 10, Middle: 10, Treble: 10).
+                This corresponds to 4 minutes of training data for the TS1 model and 12 minutes of training data for the TS3 model.
+            </p>
 
-        <div id="setting-9" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
-        </div>
+            <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Bass control varied from 0 to 10, with Middle and Treble controls set to 5 in all cases.</p>
+            <div class="w-full flex flex-row justify-around">
+                <p class="w-[7rem]"></p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 0</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 2</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 4</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 6</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 8</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Bass: 10</p>
+            </div>
 
-        <div id="setting-10" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
-        </div>
-        
-        <div id="setting-11" class="w-full flex flex-row justify-around border-b pb-2 items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
-        </div>
+            <div id="setting-6" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
+            </div>
 
-        <!-- mid -->
-        <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Treble control varied from 0 to 10, with Bass and Middle controls set to 5 in all cases.</p>
-        <div class="w-full flex flex-row justify-around">
-            <p class="w-[7rem]"></p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 0</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 2</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 4</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 6</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 8</p>
-            <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 10</p>
-        </div>
+            <div id="setting-7" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
+            </div>
+            
+            <div id="setting-8" class="w-full flex flex-row justify-around items-center border-b pb-2">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
+            </div>
 
-        <div id="setting-12" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
-        </div>
+            <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Middle control varied from 0 to 10, with Bass and Treble controls set to 5 in all cases.</p>
+            <div class="w-full flex flex-row justify-around">
+                <p class="w-[7rem]"></p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 0</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 2</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 4</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 6</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 8</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Middle: 10</p>
+            </div>
 
-        <div id="setting-13" class="w-full flex flex-row justify-around items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
-        </div>
-        
-        <div id="setting-14" class="w-full flex flex-row justify-around border-b pb-2 items-center">
-            <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
+            <div id="setting-9" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
+            </div>
+
+            <div id="setting-10" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
+            </div>
+            
+            <div id="setting-11" class="w-full flex flex-row justify-around border-b pb-2 items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
+            </div>
+
+            <p class="w-full text-base flex items-center justify-center mt-4 font-semibold border-b">Treble control varied from 0 to 10, with Bass and Middle controls set to 5 in all cases.</p>
+            <div class="w-full flex flex-row justify-around">
+                <p class="w-[7rem]"></p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 0</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 2</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 4</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 6</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 8</p>
+                <p class="flex items-center justify-center rounded-md w-[7rem] bg-neutral-200">Treble: 10</p>
+            </div>
+
+            <div id="setting-12" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-green-600 text-white">Reference</p>
+            </div>
+
+            <div id="setting-13" class="w-full flex flex-row justify-around items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS1</p>
+            </div>
+            
+            <div id="setting-14" class="w-full flex flex-row justify-around border-b pb-2 items-center">
+                <p class="flex items-center justify-center rounded-md w-[7rem] h-6 bg-violet-600 text-white">TS3</p>
+            </div>
         </div>
 
     </div>
